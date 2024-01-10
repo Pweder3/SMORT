0e826a20585e9da5c99e117ecaaa656bb6554910
pweder69 <pweder07@charlottesvilleschools.org>
2024-01-10 12:48:11 -0500

    csv integration handler started

--- Simulation Code/Integration handler/csvIntegrate.py
+++ Simulation Code/Integration handler/csvIntegrate.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pandas as pd
 
 order = 300
 x = np.linspace(start= 0, stop= 20,num= order)
-y = x**2
+y = 2*x**2 - 2*x+1
 
 
 integrated = np.array([np.trapz(y[:i],x= x[:i] ) for i in range(order) ])  
@@ -17,14 +17,14 @@ fig, ax = plt.subplots()
 
 
 # ax.plot(x, y )
-ax.plot(x, x**3/3)
+ax.plot(x, y)
 ax.plot(x, integrated) 
 plt.ylim(0,10)
 
 
 plt.xlabel('Time')
 plt.ylabel('Function Values')
-plt.legend(['y = x^3/3', 'Integrated'])
+plt.legend(['y = x**2', 'Integrated'])
 
 plt.show()
 
