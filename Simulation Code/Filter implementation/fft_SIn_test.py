b8e43601fb83ed8bf50ceb1d0606313e1ff67c7c
pweder69 <pweder07@charlottesvilleschools.org>
2024-01-25 23:13:51 -0500

    fft example

---
+++ Simulation Code/Filter implementation/fft_SIn_test.py
@@ -0,0 +1,31 @@
+
+from scipy.fft import fft, fftfreq,ifft
+import numpy as np
+import matplotlib.pyplot as plt
+
+coeficent = 5
+
+# Number of sample points
+N = 10000 
+# sample spacing
+T = 1.0 / 800.0 
+x = np.linspace(0.0, N*T, N,)
+y = np.sin(2 * 2.0*np.pi*x) + 0.2*np.sin(50 * 2 *np.pi*x)
+# y = np.sin(2 * 2.0*np.pi*x)
+
+yf = fft(y)
+xf = fftfreq(N, T)
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+
+ax1.plot(xf, np.abs(yf))
+ax1.set_xlim(-60,60)
+
+
+ax2.plot(y)
+ax2.set_xlim(0,700)
+
+plt.grid()
+plt.show()
+
+# example from https://docs.scipy.org/doc/scipy/tutorial/fft.html
