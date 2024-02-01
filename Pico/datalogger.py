1301495b0e1fb5375b2d4d8613399eb1dd79f55a
Paul Weder <112962227+Pweder69@users.noreply.github.com>
2024-02-01 14:08:30 -0500

    basic system change to lsm,

--- Pico/datalogger.py
+++ Pico/datalogger.py
@@ -13,12 +13,15 @@ i2c = busio.I2C(scl_pin, sda_pin)
 
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
+start_time = time.monotonic() # type: ignore
+pString = ""
 
 with open("/data.csv", "a") as datalog:
     #this line opens the file data.txt and appends info to the end of it
     datalog.write(f" -----, ------, ------, -------, ------ \n")
     while True:
         curTime = time.monotonic() # type: ignore
+        difference_time = curTime - start_time
         
         acc = mpu.acceleration
 
@@ -26,9 +29,11 @@ with open("/data.csv", "a") as datalog:
         
            # 0.768448, 0.181718, -1.70216
         roundValue = 2
-        pString = f" {curTime}, {added[0] -0.768448}, {added[1]-0.182517}, {added[2] +1.70052}, "
-        print(pString)
+        pString += f" {curTime}, {added[0] -0.768448}, {added[1]-0.182517}, {added[2] +1.70052}, "
         
-        datalog.write(pString + "\n")
-        datalog.flush()
+        if difference_time % 2 == 0:
+            print(pString)
+            datalog.write(pString + "\n")
+            datalog.flush()
+            pString = ""
         
--- Pico/printMpuData.py
+++ Pico/printMpuData.py
@@ -1,5 +1,5 @@
 import time
-import adafruit_mpu6050 # type: ignore  
+import adafruit_lsm6ds # type: ignore  
 import busio # type: ignore
 import board # type: ignore
 import digitalio # type: ignore
@@ -11,18 +11,30 @@ scl_pin = board.GP17
 
 i2c = busio.I2C(scl_pin, sda_pin)   
 
-mpu = adafruit_mpu6050.MPU6050(i2c)
+mpu = adafruit_lsm6ds.LSM6DSOX(i2c)
+
+start_time = time.monotonic() # type: ignore
+pString = ""
+data_amount = 0
 
 while True:
     curTime = time.monotonic() # type: ignore
+    difference_time = curTime - start_time
+    
     
     acc = mpu.acceleration
     added = [round(x,3) for x in acc ] 
-    
+    # 0x6a
     
     # 0.768448, 0.181718, -1.70216
     
+    
     roundValue = 2
-    pString = f" {curTime}, {added[0] -0.768448}, {added[1]-0.182517}, {added[2] +1.70052}, "
-    print(pString)
+    pString += f" {curTime}, {added[0] -0.768448}, {added[1]-0.182517}, {added[2] +1.70052}, " + "\n"
+    data_amount +=1
+    print(data_amount/difference_time)
+    
+    if round(difference_time,1) % 2 == 0:
+        pString = ""
+    
         
