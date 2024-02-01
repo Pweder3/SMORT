a8a6b3eea89de4935efa301c1a9efc85fc920763
pweder69 <pweder07@charlottesvilleschools.org>
2024-02-01 13:26:20 -0500

    images added

--- Simulation Code/Filter implementation/fft_SIn_test.py
+++ Simulation Code/Filter implementation/fft_SIn_test.py
@@ -2,6 +2,8 @@
 from scipy.fft import fft, fftfreq,ifft
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import savefig
+plt.style.use('dark_background')
 
 coeficent = 5
 
@@ -20,12 +22,17 @@ fig, (ax1, ax2) = plt.subplots(2, 1)
 
 ax1.plot(xf, np.abs(yf))
 ax1.set_xlim(-60,60)
-
+ax1.grid()
+ax1.set_title('Fourier transform')
 
 ax2.plot(y)
 ax2.set_xlim(0,700)
+ax2.grid()
+ax2.set_title('Signal')
 
-plt.grid()
+plt.subplots_adjust(hspace=0.5)
+# plt.savefig('docs/Images/Images/fft_sin.png', dpi=300,transparent = True)
 plt.show()
+
 
 # example from https://docs.scipy.org/doc/scipy/tutorial/fft.html
